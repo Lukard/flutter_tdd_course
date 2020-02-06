b50a4332ba722cb322e008c6979db8a390ae5acb
Rubén Abad <rubenabad89@gmail.com>
2020-02-06 19:03:42 +0100

    Change fixture reader to be able to work with CI

--- test/fixture/fixture_reader.dart
+++ test/fixture/fixture_reader.dart
@@ -1,3 +1,9 @@
 import 'dart:io';
 
-String fixture(String name) => File('./test/fixture/$name').readAsStringSync();
+String fixture(String name) {
+  Directory current = Directory.current;
+  String path =
+      current.path.endsWith('/test') ? current.path : current.path + '/test';
+
+  return File('$path/fixture/$name').readAsStringSync();
+}
